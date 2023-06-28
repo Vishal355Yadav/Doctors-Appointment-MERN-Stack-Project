a2f32ffbcba94b855c42f6f65fc9d8212e05ff8d
Vishal355Yadav <201b355@juetguna.in>
2023-06-28 10:06:24 +0530

    basic layout done

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import './App.css';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
+function App() {
+  return (
+   <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element= {<HomePage/>}></Route>
+        <Route path='/login' element= {<Login/>}></Route>
+        <Route path='/register' element= {<Register/>}></Route>
+      </Routes>
+    </BrowserRouter>
+   </>
+  );
+}
+
+export default App;
