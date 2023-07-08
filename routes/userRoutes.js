9e0809adcffb182f030100769cf35ae34b419c0a
Vishal355Yadav <201b355@juetguna.in>
2023-07-08 08:36:57 +0530

    authorization complete for backend

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express= require('express');
-const { loginController,registerController } = require('../controllers/userCtrl');
+const { loginController,registerController, authController } = require('../controllers/userCtrl');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router= express.Router();
 
 router.post('/login',loginController);
 
 router.post('/register',registerController);
-module.exports =router;
+router.post('/getUserData',authMiddleware,authController)
+module.exports = router;
